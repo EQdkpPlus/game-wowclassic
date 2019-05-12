ca5f48c1f0a7faa6b3493fad9a8f1504923cc1b6
GodMod <godmod23@web.de>
2019-05-12 19:51:14 +0200

    Fixed template path

--- infotooltip/wowhead.class.php
+++ infotooltip/wowhead.class.php
@@ -21,59 +21,59 @@
 
 if(!class_exists('wowhead')) {
 	class wowhead extends itt_parser {
-
+		
 		public static $shortcuts = array('puf' => 'urlfetcher');
-
+		
 		public $av_langs = array('classic' => 'Classic', 'en' => 'en_US', 'de' => 'de_DE', 'fr' => 'fr_FR', 'ru' => 'ru_RU', 'es' => 'es_ES');
-
+		
 		public $settings = array(
-			'itt_icon_loc' => array(
-				'type' => 'text',
-				'default' => 'https://wow.zamimg.com/images/wow/icons/large/'
-			),
-			'itt_icon_small_loc' => array(
-				'type' => 'text',
-				'default' => 'https://wow.zamimg.com/images/wow/icons/small/'
-			),
-			'itt_icon_ext' => array(
-				'type' => 'text',
-				'default' => '.jpg'
-			),
-			'itt_default_icon' => array(
-				'type' => 'text',
-				'default' => 'inv_misc_questionmark'
-			)
+				'itt_icon_loc' => array(
+						'type' => 'text',
+						'default' => 'https://wow.zamimg.com/images/wow/icons/large/'
+				),
+				'itt_icon_small_loc' => array(
+						'type' => 'text',
+						'default' => 'https://wow.zamimg.com/images/wow/icons/small/'
+				),
+				'itt_icon_ext' => array(
+						'type' => 'text',
+						'default' => '.jpg'
+				),
+				'itt_default_icon' => array(
+						'type' => 'text',
+						'default' => 'inv_misc_questionmark'
+				)
 		);
-
+		
 		private $searched_langs = array();
-
+		
 		public function __destruct(){
 			unset($this->searched_langs);
 			parent::__destruct();
 		}
-
+		
 		protected function searchItemID($itemname, $lang, $searchagain=0) {
 			$searchagain++;
 			$this->pdl->log('infotooltip', 'wowhead->searchItemID called: itemname: '.$itemname.', lang: '.$lang.', searchagain: '.$searchagain);
 			$item_id = 0;
-
+			
 			// Ignore blank names.
 			$name = trim($itemname);
 			if (empty($name)) { return null; }
-
+			
 			$encoded_name = urlencode($name);
 			$encoded_name = str_replace('+' , '%20' , $encoded_name);
 			$lang_prefix = ($lang == 'en') ? 'www' : $lang;
 			
 			//$lang_prefix = "classic";
-
+			
 			$item_encoded_name = $encoded_name;
 			$item_encoded_name = str_replace("%C3%", "%C3%20%", $item_encoded_name);
-
+			
 			$url = 'https://'.$lang_prefix.'.wowhead.com/item='.$item_encoded_name.'&xml';
 			$this->pdl->log('infotooltip', 'Search for ItemID at '.$url);
 			$item_data = $this->puf->fetch($url);
-
+			
 			$xml = simplexml_load_string($item_data);
 			if(is_object($xml) && !isset($xml->error)) {
 				$item_id = (int)$xml->item->attributes()->id;
@@ -95,7 +95,7 @@ if(!class_exists('wowhead')) {
 					$item_id = $arrUniqueIDs[0];
 				}
 			}
-
+			
 			//search in other languages
 			if(!$item_id AND $searchagain < count($this->av_langs)) {
 				$this->pdl->log('infotooltip', 'No Items found.');
@@ -113,17 +113,17 @@ if(!class_exists('wowhead')) {
 			$this->pdl->log('infotooltip', $debug_out);
 			return array($item_id, 'items');
 		}
-
+		
 		protected function getItemData($item_id, $lang, $itemname='', $type='items'){
 			$orig_id = $item_id;
-
+			
 			if(!$item_id) {
 				$item['baditem'] = true;
 				return $item;
 			}
-
+			
 			$bonus = 0;
-
+			
 			$myItemData = array(
 					'ench' => 0,
 					'gems' => array(),
@@ -134,20 +134,20 @@ if(!class_exists('wowhead')) {
 					'num_bonus' => 0,
 					'upgd_id' => '',
 			);
-
+			
 			$arrItemData = explode(':', $item_id);
 			if (count($arrItemData) > 1){
 				$item_id = $arrItemData[0];
-
+				
 				//Detect if old or new (6.2.0) format
 				$intTotalCount = count($arrItemData);
 				$intBonusCount = $arrItemData[12];
 				$isUpgrade = ((int)$arrItemData[10] != 0) ? true : false;
 				$intShouldCount = 13 + $intBonusCount + (($isUpgrade) ? 1 : 0);
 				$blnIsNewFormat = ($intShouldCount === $intTotalCount) ? true : false;
-
+				
 				$arrBonus = array();
-
+				
 				if($blnIsNewFormat){
 					foreach($arrItemData as $key => $val){
 						if ($key == 1) $myItemData['ench'] = $val;
@@ -159,15 +159,15 @@ if(!class_exists('wowhead')) {
 						if ($key > 2 && $key < 7){
 							$myItemData['gems'][] = $val;
 						}
-
+						
 						if ($key > 12 && $key < (13+$myItemData['num_bonus'])){
 							$myItemData['bonus'][] = $val;
 						}
-
+						
 						if($key > (12+$myItemData['num_bonus'])){
 							$myItemData['upgd_id'] = $val;
 						}
-
+						
 						//124139:0      :     0:     0:     0:     0:       0:       0:      100:           267:              0:                   6:          1:     567
 						//itemID:enchant:gemID1:gemID2:gemID3:gemID4:suffixID:uniqueID:linkLevel:specializationID:upgradeTypeID:instanceDifficultyID:numBonusIDs:bonusID1:bonusID2
 					}
@@ -179,7 +179,7 @@ if(!class_exists('wowhead')) {
 						if ($key > 2 && $key < 7){
 							$myItemData['gems'][] = $val;
 						}
-
+						
 						if ($key > 11){
 							$myItemData['bonus'][] = $val;
 						}
@@ -188,22 +188,22 @@ if(!class_exists('wowhead')) {
 					//itemID:enchant:gem1:gem2:gem3:gem4:suffixID:uniqueID:level:upgradeId:instanceDifficultyID:numBonusIDs:bonusID1:bonusID2...
 				}
 			}
-
+			
 			$item = array('id' => $item_id, 'origid' => $orig_id);
 			$url = ($lang == 'en') ? 'www' : $lang;
 			
 			$url = 'classic';
-
+			
 			$item['link'] = 'https://'.$url.'.wowhead.com/tooltip/item/'.$item['id'].'&json&power&bonus='.implode(':', $myItemData['bonus']).'&upgd='.$myItemData['upgd_id'].'&lvl='.$myItemData['lvl'].'&ench='.$myItemData['ench'].'&gems='.implode(',',$myItemData['gems']);
 			
 			$this->pdl->log('infotooltip', 'fetch item-data from: '.$item['link']);
 			$someJS = $this->puf->fetch($item['link'], array('Cookie: cookieLangId="'.$lang.'";'));
-
+			
 			if ($someJS){
 				$arrMatches = array();
 				
 				//$intCount = preg_match("/name_(.*):\"(.*)\",(\s*)\"quality\":(.*),(\s*)\"icon\":\"(.*)\",(\s*)\"tooltip_(.*)\":\"(.*)\"/", $someJS, $arrMatches);
-
+				
 				$intCount = preg_match('/name":"(.*)",/U', $someJS, $arrMatches);
 				if ($intCount){
 					$item['name'] = htmlentities(stripslashes(json_decode('"'.$arrMatches[1].'"')));
@@ -221,16 +221,18 @@ if(!class_exists('wowhead')) {
 					//Tooltip
 					$arrMatches = array();
 					preg_match('/tooltip":"(.*)"(,"|})/', $someJS, $arrMatches);
+					
 					$ahtml = $arrMatches[1];
 					
-					$template_html = trim(file_get_contents($this->root_path.'games/wow/infotooltip/templates/wow_popup.tpl'));
-
+					$template_html = trim(file_get_contents($this->root_path.'games/wowclassic/infotooltip/templates/wowclassic_popup.tpl'));
+					
 					$html = json_decode('"'.$ahtml.'"');
+					
 					if($html == NULL){
 						$html = preg_replace('/","(.*)/', "", $ahtml);
 						$html = json_decode('"'.$html.'"');
 					}
-
+					
 					$item['html'] = str_replace('{ITEM_HTML}', stripslashes($html), $template_html);
 					$item['lang'] = $lang;
 					
@@ -238,7 +240,7 @@ if(!class_exists('wowhead')) {
 					$item['id'] = $orig_id;
 					
 					return $item;
-
+					
 				} else {
 					$this->pdl->log('infotooltip', 'no match found');
 					$item['baditem'] = true;
@@ -247,14 +249,14 @@ if(!class_exists('wowhead')) {
 			} else {
 				$this->pdl->log('infotooltip', 'no data from URL');
 				$item['baditem'] = true;
-
+				
 				return $item;
 			}
-
+			
 			$item['baditem'] = true;
 			return $item;
 		}
-
+		
 	}
 }
 ?>
